cd778d6ea4ae75e3094c2ad5085331229a47db44
takshakmudgal <takshakmudgal@gmail.com>
2025-04-08 03:15:37 +0530

    feat: integrated x auth

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // Warning: this disables ESLint during builds
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // Warning: this disables TypeScript type checking during builds
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  eslint: {
+    // Warning: this disables ESLint during builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: this disables TypeScript type checking during builds
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["pbs.twimg.com"], // Allow Twitter profile images
+  },
+};
+
+module.exports = nextConfig;
