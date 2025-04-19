191e9552ab09820894b514091a4604570dc98455
arv0098 <arvind@batoncorporation.com>
2025-04-19 16:14:44 +0530

    use connection from hook

---
+++ app/hooks/useConnection.ts
@@ -0,0 +1,20 @@
+import { Connection } from "@solana/web3.js";
+import { useState, useEffect } from "react";
+
+const useConnection = () => {
+  const [connection, setConnection] = useState<Connection>(
+    new Connection("api.devnet.solana.com")
+  );
+
+  useEffect(() => {
+    const con = new Connection(
+      process.env.NEXT_PUBLIC_SOLANA_CLUSTER_URL || "",
+      "confirmed"
+    );
+    setConnection(con);
+  }, []);
+
+  return connection;
+};
+
+export default useConnection;
