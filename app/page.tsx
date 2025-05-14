9f77f3b406832a406dc915df85044aa73058c102
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2025-05-14 13:49:05 +0530

    Adds GA to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+'use client'
+
 import { MemeCoinWars } from "@/components/meme-coin-wars"
+import { useEffect } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
+import ReactGA from 'react-ga4'
 
 export default function Home() {
+  console.log('Home is loading for GA');
+
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const GA_MEASUREMENT_ID = "G-P2ZFKY4WCV"
+
+  useEffect(() => {
+    if (GA_MEASUREMENT_ID) {
+      ReactGA.initialize(GA_MEASUREMENT_ID)
+      
+      ReactGA.send({ hitType: "pageview", page: pathname + searchParams.toString() })
+      
+    }
+  }, [pathname, searchParams])
+
   return (
     <div className="flex-1">
       <MemeCoinWars />
